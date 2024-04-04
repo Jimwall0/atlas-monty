dff04d0c1b85e35505d72351527f705b1d5bd5f1
Tsimmuaj <tyang4204@gmail.com>
2024-04-04 09:20:53 -0500

    finished find_function.c

--- find_function.c
+++ find_function.c
@@ -3,7 +3,19 @@
  * find_function - finds a function based of input
  * Return: pointer function
 */
-int find_function(char *string)
+void *find_function(char *string)
 {
-    
+	int number = 0;
+	instruction_t fab = {
+		{"push", push};
+		{"pull", pull};
+	};
+
+	while (number < 2)/*loops through the struct to match strings*/
+	{
+		if (strcmp(string, fab[number].opcode) == 0)
+			return (fab[number].f);
+		number++;
+	}
+	return(NULL);
 }
--- main.h
+++ main.h
@@ -35,6 +35,6 @@ typedef struct instruction_s
 } instruction_t;
 
 int user_input(char num, char **buffer, size_t size);
-int find_function(char *string);
+void *find_function(char *string);
 
 #endif
